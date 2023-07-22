5b56284c1012c461d58c9c3216c57af95d92982a
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-22 20:20:30 +0200

    add ellipse

---
+++ ellipse.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import {
+  Mafs,
+  Ellipse,
+  Circle,
+  Coordinates,
+  useMovablePoint,
+  Theme,
+  vec,
+  Transform,
+} from "mafs"
+import { createRender, useModelState } from "@anywidget/react";
+import "mafs/core.css";
+
+export const render = createRender(() => {
+  const [content] = useModelState("content");
+  return <App content={content} />;
+});
+
+
+function App({ content }) {
+  const hintRadius = 3;
+
+  // This center point translates everything else.
+  const translate = useMovablePoint([0, 0], {
+    color: Theme.orange,
+  });
+
+  // This outer point rotates the ellipse, and
+  // is also translated by the center point.
+  const rotate = useMovablePoint([hintRadius, 0], {
+    color: Theme.blue,
+    // Constrain this point to only move in a circle
+    constrain: (position) => vec.withMag(position, hintRadius),
+  });
+  const angle = Math.atan2(rotate.point[1], rotate.point[0]);
+
+  const width = useMovablePoint([-2, 0], {
+    constrain: "horizontal",
+  });
+  const height = useMovablePoint([0, 1], {
+    constrain: "vertical",
+  });
+  return (
+    <Mafs viewBox={{ x: [-3, 3], y: [-3, 3] }}>
+      <Coordinates.Cartesian />
+
+      <Transform translate={translate.point}>
+        <Transform rotate={angle}>
+          {/*
+           * Display a little hint that the
+           * point is meant to move radially
+           */}
+          <Circle
+            center={[0, 0]}
+            radius={hintRadius}
+            strokeStyle="dashed"
+            strokeOpacity={0.3}
+            fillOpacity={content}
+          />
+
+          <Ellipse
+            center={[0, 0]}
+            radius={[Math.abs(width.x), Math.abs(height.y)]}
+          />
+
+          {width.element}
+          {height.element}
+        </Transform>
+
+        {rotate.element}
+      </Transform>
+
+      {translate.element}
+    </Mafs>
+  );
+}
--- src/ipymafs/__init__.py
+++ src/ipymafs/__init__.py
@@ -2,7 +2,7 @@ import pathlib
 import anywidget
 import traitlets
 
-class Mafs(anywidget.AnyWidget):
-    content = traitlets.Unicode("Hi").tag(sync=True)
-    _esm =  pathlib.Path(__file__).parent / "widget.js"
-    _css=  pathlib.Path(__file__).parent / "widget.css"
+class Ellipse(anywidget.AnyWidget):
+    content = traitlets.Float(0).tag(sync=True)
+    _esm =  pathlib.Path(__file__).parent / "ellipse.js"
+    _css=  pathlib.Path(__file__).parent / "ellipse.css"
