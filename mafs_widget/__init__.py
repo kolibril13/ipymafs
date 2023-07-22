a642f6a1cc8b41163ad062fb263bd5db1f371d09
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-22 11:46:45 +0200

    remove all

--- mafs_widget/__init__.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-import anywidget
-import traitlets
-
-class Mafs(anywidget.AnyWidget):
-    content = traitlets.Unicode("Hi").tag(sync=True)
-    _esm =  pathlib.Path(__file__).parent / "static/widget.js"
-
-    _css=  pathlib.Path(__file__).parent / "static/widget.css"
